61767c58adb358df30dfead976216e678ab50008
Matías Rodriguez <matiasemanuelr98@gmail.com>
2021-12-09 20:57:58 -0300

    <feat>(Adoption): create code
    se desarrollo el contrato junto al test

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Adoption { }
+contract Adoption { 
+
+    address[16] public adopters;
+
+    function getAdopters() public view returns (address[16] memory){
+        return adopters;
+    }
+
+    function adopt(uint petId) public returns(uint){
+        require(petId>= 0 && petId <= 15, "Invalid pet id");
+        adopters[petId] = msg.sender;
+        return petId;
+    } 
+
+    function getAdopterOf(uint petId) public view returns (address){
+        return adopters[petId];
+    }
+}
